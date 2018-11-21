dfc8d70ffe9b4630d1be4704c7ac34009ba46b28
qq495180585 <495180585@qq.com>
2018-11-21 19:41:14 +0800

    首页公共部分完成

--- public/back1/js/common.js
+++ public/back1/js/common.js
@@ -5,4 +5,34 @@ $(function () {
     $(document).ajaxStop(function () {
         NProgress.done();
     })
+    // 左侧二级切换功能
+    $('.lt-aside .nav ul li .category').on('click', function () {
+        $('.lt-aside .nav ul li .classify').stop().slideToggle();
+    })
+    // 右上左按钮
+    $('.lt-main .lt-topbar .pull-left').on('click', function () {
+        $('.lt-aside').toggleClass('hidemenu');
+        $('.lt-main .lt-topbar').toggleClass('hidemenu');
+        $('.lt-main').toggleClass('hidemenu');
+    })
+    // 退出登录功能
+    $('.lt-main .lt-topbar .pull-right').on('click', function () {
+        $('#myModal').modal('show');
+    })
+    $('.btn-out').on('click', function () {
+        $.ajax({
+            type: 'get',
+            url: '/employee/employeeLogout',
+            dataType: 'json',
+            success: function (res) {
+                if (res.success) {
+                    location.href = 'login.html';
+                }
+                if (res.error) {
+                    console.log('操作失败');
+                }
+
+            }
+        })
+    })
 })
